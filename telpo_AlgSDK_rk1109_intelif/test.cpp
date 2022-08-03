3730746f00b26ff76ae4bb7a1da7ef6c02f60cd8
maxsunyc <maxsunyc@gmail.com>
2022-08-03 15:24:28 +0800

    add telpo_AlgSDK_rk1109_intelif

---
+++ telpo_AlgSDK_rk1109_intelif/test.cpp
@@ -0,0 +1,18 @@
+/*
+ * @Copyright: Guangdong Telpo Technologies
+ * @Author: sun.yongcong(maxsunyc@gmail.com)
+ * @Date: 2022-07-01 17:42:50
+ * @Description: Algorithm API for image&video analysis
+ */
+
+#include<iostream>
+#include<fstream>
+#include<string>
+#include "3rdparty/nlohmann/json.hpp"
+int main()
+{
+    uint8_t x=135;
+    std::cout<<"\nx="<<unsigned(x)<<std::endl;
+
+    return 0;
+}
